e0ce175310e02882ffb8172b723782915ecacee2
listerineh <jsalvarez4@espe.edu.ec>
2022-07-01 02:11:23 -0500

    create components fot CRUD operations and format the routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './layout/Layout'
 import Home from './pages/Home'
 import Employees from './pages/Employees'
+import NewEmployeeForm from './pages/NewEmployeeForm'
+import EditEmployeeForm from './pages/EditEmployeeForm'
 import Providers from './pages/Providers'
+import NewProviderForm from './pages/NewProviderForm'
+import EditProviderForm from './pages/EditProviderForm'
 import Equipment from './pages/Equipment'
+import NewEquipmentForm from './pages/NewEquipmentForm'
+import EditEquipmentForm from './pages/EditEquipmentForm'
+import Error404 from './pages/Error404'
 
 function App() {
 
@@ -13,15 +20,16 @@ function App() {
         <Route path='' element={<Layout />} >
           <Route index element={<Home />} />
           <Route path='employees' element={<Employees />} />
-          <Route path='employees/new' element={<Employees />} />
-          <Route path='employees/edit/:id' element={<Employees />} />
+          <Route path='employees/new' element={<NewEmployeeForm />} />
+          <Route path='employees/edit/:id' element={<EditEmployeeForm />} />
           <Route path='providers' element={<Providers />} />
-          <Route path='providers/new' element={<Providers />} />
-          <Route path='providers/edit/:id' element={<Employees />} />
+          <Route path='providers/new' element={<NewProviderForm />} />
+          <Route path='providers/edit/:id' element={<EditProviderForm />} />
           <Route path='equipment' element={<Equipment />} />
-          <Route path='equipment/new' element={<Equipment />} />
-          <Route path='equipment/edit/:id' element={<Employees />} />
+          <Route path='equipment/new' element={<NewEquipmentForm />} />
+          <Route path='equipment/edit/:id' element={<EditEquipmentForm />} />
         </Route>
+        <Route path='*' element={<Error404 />}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/NewProviderForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewProviderForm = () => {
+  return (
+    <div>NewProviderForm</div>
+  )
+}
+
+export default NewProviderForm
